9318cc7e5f5548705d4dd02bf649969a988f9bdd
Ioana Rusan <ioana.rusan@student.upt.ro>
2022-05-27 20:10:28 +0300

    added header template

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter, Router } from "react-router-dom";
 import { App } from "./App";
+import { Header } from "./components/common/header/header";
 import { AuthProvider } from "./context/AuthProvider";
 import "./styles.css";
 
 ReactDOM.render(
   <BrowserRouter>
     <AuthProvider>
+        <Header />
       <App />
     </AuthProvider>
   </BrowserRouter>,
