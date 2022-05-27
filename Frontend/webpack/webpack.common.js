bcd656e3ba77b3eb679c71797c92b9c6153c59cd
Catalin Candea <catalin.candea@student.upt.ro>
2022-05-27 20:02:40 +0300

    Acum sper sa am loaderu

--- Frontend/webpack/webpack.common.js
+++ Frontend/webpack/webpack.common.js
@@ -29,6 +29,17 @@ module.exports = {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
         type: "asset/inline",
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
     ],
   },
   output: {
