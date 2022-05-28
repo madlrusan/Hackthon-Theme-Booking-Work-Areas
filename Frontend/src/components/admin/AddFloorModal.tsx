80fb71c23d2a8c59fd73683bcf78a731a1e138cd
Catalin Candea <catalin.candea@student.upt.ro>
2022-05-28 22:00:31 +0300

    Add grid

--- Frontend/src/components/admin/AddFloorModal.tsx
+++ Frontend/src/components/admin/AddFloorModal.tsx
@@ -2,15 +2,38 @@ import { Box, Modal, Typography } from "@mui/material";
 import { FC, useContext, useEffect, useState } from "react";
 import { ModalsContext } from "../../context/ModalProvider";
 import { Desk } from "../../models/Desk";
+import FloorGrid, { deskCoordinates } from "../common/floorGrid.tsx/floorGrid";
 
 export const AddFloorModal: FC = () => {
   const modalsContext = useContext(ModalsContext);
-
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     modalsContext.setAddFloorOpen(false);
   };
   const [desks, setDesks] = useState<Desk[]>();
+
+  const [rows, setRows] = useState(0);
+  const [columns, setColumns] = useState(0);
+  const populateDesks = (hoteledDesks: deskCoordinates[]) => {
+    let desksList: Desk[] = [];
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        const isHoteled = hoteledDesks.filter((desk) => {
+          return desk.row === i && desk.column === j;
+        });
+
+        const desk: Desk = {
+          id: 0,
+          floorId: 0,
+          name: `${i} ${j}`,
+          isHotelingDesk: isHoteled.length > 0 ? true : false,
+          reserved: false,
+        };
+        desksList.push(desk);
+      }
+    }
+    setDesks(desksList);
+  };
   return (
     <Modal
       open={modalsContext.isAddFloorOpen}
@@ -30,14 +53,35 @@ export const AddFloorModal: FC = () => {
             </span>
           </div>
           <label htmlFor="deskRow">Desk rows</label>
-          <input type="number" id="deskRow" />
+          <input
+            type="number"
+            id="deskRow"
+            max-value="10"
+            onChange={(e) => {
+              setRows(parseInt(e.target.value));
+            }}
+          />
           <label htmlFor="deskColumn">Desk columns</label>
-          <input type="number" id="deskColumn" />
+          <input
+            type="number"
+            id="deskColumn"
+            max-value="10 "
+            onChange={(e) => {
+              setColumns(parseInt(e.target.value));
+            }}
+          />
         </form>
         <div>
           <h2>Location map</h2>
           <span>Chose offices that are permanently booked.</span>
         </div>
+        <FloorGrid
+          rows={rows}
+          columns={columns}
+          saveDesks={(hoteledDesks: deskCoordinates[]) => {
+            populateDesks(hoteledDesks);
+          }}
+        />
         <div className="btn">
           <button className="submitFormButton" disabled={false}>
             Save floor
--- Frontend/src/components/common/floorGrid.tsx/floorGrid.tsx
+++ Frontend/src/components/common/floorGrid.tsx/floorGrid.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
 import styled from "styled-components";
+import "./floorGrid.scss";
 import { Container, Row, Col } from "react-bootstrap";
 import { Grid } from "@mui/material";
 // import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,20 +8,73 @@ import { Grid } from "@mui/material";
 type FloorGridProps = {
   rows: number;
   columns: number;
+  saveDesks: any;
 };
-const Item = () => {
-  return <div className="desk">birou</div>;
+type ItemProps = {
+  row: number;
+  column: number;
+  bookDesk: any;
+  bookedDesks: deskCoordinates[];
+};
+export type deskCoordinates = {
+  row: number;
+  column: number;
+};
+const Item: FC<ItemProps> = ({ row, column, bookDesk, bookedDesks }) => {
+  const [isBooked, setIsBooked] = useState<Boolean | null>();
+  useEffect(() => {
+    const booked = bookedDesks.filter((desk) => {
+      return desk.row === row && desk.column === column;
+    });
+    if (booked.length > 0) {
+      setIsBooked(true);
+    }
+  }, [bookedDesks]);
+
+  return (
+    <div
+      className={isBooked === true ? " desk booked" : "desk"}
+      onClick={() => {
+        if (isBooked) {
+          setIsBooked(!isBooked);
+        }
+        bookDesk(isBooked, row, column);
+      }}
+    >
+      {row},{column}
+    </div>
+  );
 };
 
 const FloorGrid = (props: FloorGridProps) => {
-  const { rows, columns } = props;
-  const renderColumns = (columns: number) => {
+  const [bookedDesks, setBookedDesks] = useState<deskCoordinates[]>([]);
+  const bookDesk = (isBooked: boolean, row: number, column: number) => {
+    if (isBooked) {
+      // debugger;
+      const newBookedDesks = bookedDesks.filter((desk) => {
+        console.log(desk);
+        return desk.row !== row || desk.column !== column;
+      });
+      setBookedDesks(newBookedDesks);
+    } else {
+      setBookedDesks([...bookedDesks, { row, column }]);
+    }
+  };
+  const { rows, columns, saveDesks } = props;
+  const renderColumns = (row: number, columns: number) => {
     let columnComponents: any = [];
     for (let i = 0; i < columns; i++) {
       columnComponents.push(
         <Col key={i} xs="auto">
-          {" "}
-          <Item />{" "}
+          <Item
+            // key={i}
+            row={row}
+            column={i}
+            bookDesk={(isBooked: boolean, row: number, column: number) =>
+              bookDesk(isBooked, row, column)
+            }
+            bookedDesks={bookedDesks}
+          />
         </Col>
       );
     }
@@ -30,22 +84,27 @@ const FloorGrid = (props: FloorGridProps) => {
     let rowComponents: any = [];
     for (let i = 0; i < rows; i++) {
       rowComponents.push(
-        <Row>
-          <div>{renderColumns(columns)} </div>
+        <Row key={i}>
+          <div style={{ flexDirection: "row", display: "flex" }}>
+            {renderColumns(i, columns)}{" "}
+          </div>
         </Row>
       );
     }
     return rowComponents;
   };
   return (
-    <Grid
-      container
-      className="grid-container"
-      spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 4, sm: 8, md: 12 }}
-    >
-      {renderRows(rows, columns)}
-    </Grid>
+    <div style={{ padding: "20px" }}>
+      <Grid
+        container
+        className="grid-container"
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {renderRows(rows, columns)}
+        <button onClick={() => saveDesks(bookedDesks)}>Save desks</button>
+      </Grid>
+    </div>
   );
 };
 
