4f54e1a7ad9bfa4dce6c00192ab8871ed0207856
Adelin Chis <adelin.chis@theaccessgroup.com>
2022-05-29 02:26:10 +0300

    Get floor by id with desks query

---
+++ backend/ITEC.Backend.Persistence/Repositories/Abstractions/IFloorRepository.cs
@@ -0,0 +1,14 @@
+﻿using ITEC.Backend.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITEC.Backend.Persistence.Repositories.Abstractions
+{
+    public interface IFloorRepository : IRepository<Floor>
+    {
+        Task<Floor> GetFloorWithDesksById(int floorId);
+    }
+}
