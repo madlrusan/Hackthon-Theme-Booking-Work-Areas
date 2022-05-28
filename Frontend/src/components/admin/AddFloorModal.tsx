46a6056079d0b924c04f1e52093c2b8f11b4a0cd
Ioana Rusan <ioana.rusan@student.upt.ro>
2022-05-28 20:24:37 +0300

    adding floor modal

--- Frontend/src/components/admin/AddFloorModal.tsx
+++ Frontend/src/components/admin/AddFloorModal.tsx
@@ -2,7 +2,7 @@ import { Box, Modal, Typography } from "@mui/material";
 import { FC, useContext, useEffect, useState } from "react";
 import { ModalsContext } from "../../context/ModalProvider";
 import { Desk } from "../../models/Desk";
-
+import "./AddFloorModal.scss";
 export const AddFloorModal: FC = () => {
   const modalsContext = useContext(ModalsContext);
 
@@ -12,51 +12,49 @@ export const AddFloorModal: FC = () => {
   };
   const [desks, setDesks] = useState<Desk[]>();
   return (
-    <Modal
-      open={modalsContext.isAddFloorOpen}
-      onClose={() => modalsContext.setAddFloorOpen(false)}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={style}>
-        <form>
-          <label htmlFor="floorName">Floor Name</label>
-          <input type="text" id="floorName" />
-          <div>
-            <h2>Note:</h2>
-            <span>
-              The office will be displayed as a row x column matrix based on
-              your input
-            </span>
+    <div className="modal-form">
+      <Modal
+        open={modalsContext.isAddFloorOpen}
+        onClose={() => modalsContext.setAddFloorOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className="form"
+      >
+        <section className="addFloor">
+          <form>
+            <div className="FormField">
+              <label htmlFor="floorName">Floor Name: </label>
+              <input type="text" id="floorName" />
+            </div>
+            <div>
+              <h2>Note:</h2>
+              <span>
+                The office will be displayed as a row x column matrix based on
+                your input
+              </span>
+            </div>
+            <div className="FormField">
+              <label htmlFor="deskRow">Desk rows: </label>
+              <input type="number" id="deskRow" />
+            </div>
+            <div className="FormField">
+              <label htmlFor="deskColumn">Desk columns: </label>
+              <input type="number" id="deskColumn" />
+            </div>
+          </form>
+          <div className="file-adding">
+            <div className="Field">
+              <h2>Location map</h2>
+              <span>Chose offices that are permanently booked.</span>
+            </div>
+            <div className="btn">
+              <button className="submitFormButton" disabled={false}>
+                Save floor
+              </button>
+            </div>
           </div>
-          <label htmlFor="deskRow">Desk rows</label>
-          <input type="number" id="deskRow" />
-          <label htmlFor="deskColumn">Desk columns</label>
-          <input type="number" id="deskColumn" />
-        </form>
-        <div>
-          <h2>Location map</h2>
-          <span>Chose offices that are permanently booked.</span>
-        </div>
-        <div className="btn">
-          <button className="submitFormButton" disabled={false}>
-            Save floor
-          </button>
-        </div>
-      </Box>
-    </Modal>
+        </section>
+      </Modal>
+    </div>
   );
-};
-
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-  height: "70vh",
-  width: "70vw",
 };
